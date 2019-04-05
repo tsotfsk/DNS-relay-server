eb50fd1b21c21f9c49c39cbb05a97c12f645b67f
tsotfsk <1337990880@qq.com>
2019-04-05 18:00:17 +0800

    test

---
+++ TestClient.py
@@ -0,0 +1,16 @@
+from socket import * 
+
+if __name__ == "__main__":
+    addr = ('127.0.0.1', 60000)
+    testClient = socket(AF_INET, SOCK_DGRAM,0)
+    while True:
+        inp = input('数据：').strip()
+        if inp == 'exit':
+            break
+        testClient.sendto(bytes(inp, encoding = "utf-8"), addr)
+        testClient.sendto(bytes(inp, encoding = "utf-8"), addr)
+        testClient.sendto(bytes(inp, encoding = "utf-8"), addr)
+        
+        data, address = testClient.recvfrom(1024)
+        print('recv {}'.format(data.decode()))
+    testClient.close()
