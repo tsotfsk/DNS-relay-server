158dca67614d42d981ebfdb1493ed4026dd23c02
tsotfsk <1337990880@qq.com>
2019-05-11 12:57:08 +0800

    server的合包进行了更改

--- TestServer.py
+++ TestServer.py
@@ -16,7 +16,7 @@ class DnsHandler(BaseRequestHandler):
             m.fromStr(message)
         except Exception:
             return
-
+            
         if m.header.answer == 0:  # 查询包
             print(m.header.id, m.queries[0].name, m.queries[0].type, m.header.qdCount, m.header.anCount)
             self.handleRequest(m)         
@@ -32,17 +32,21 @@ class DnsHandler(BaseRequestHandler):
             sqlStr = 'select * from  DNS where NAME = ? and TYPE = ?'
             value = (m.queries[0].name.name, CNAME)
             result = database.fetchall(sqlStr, value)  
-            print(result) 
-            rr = ResourceRecord(result[0], result[1], result[2], result[3], result[])                
+            print(result)
+                         
             if len(result) == 0:  # 查不到结果就把请求转发出去
                 message = self.transform(m)  
                 self.relay(message, (ADDR, PORT))
             else:  # 自己pack包
-                m.addAnswer(rr)
+                # 整理包
+                for item in result:
+                    print(item[0], item[1], item[2], item[3], item[4])
+                    rr = database.toRR(item)
+                    m.addAnswer(rr)
                 m.answer = 1
                 # TODO 改包的header之类的
                 message = m.toStr()
-                self.relay(message, slef.clientAddress)
+                self.relay(message, self.clientAddress)
                 
     def handleResponse(self, m):
         message, addr, timeStamp = self.inverseTransform(m)  # 反变换得到ip和消息
@@ -59,7 +63,7 @@ class DnsHandler(BaseRequestHandler):
                     return
             for rr in m.answers:    
                 # TODO 放到数据库
-                sqlStr = 'insert into DNS values (?,?,?,?,?,?)'
+                sqlStr = 'insert into DNS values (?,?,?,?,?)'
                 if rr.type == MX:
                     rdata = str(rr.rdata.preference) + '|' + rr.rdata.name.name               
                 elif rr.type == A:
@@ -67,35 +71,37 @@ class DnsHandler(BaseRequestHandler):
                 else:
                     rdata = rr.rdata.name.name
                 print(rdata)
-                value = (rr.name.name, rr.type, rr.cls, rr.ttl, rr.rdlength, rdata)
+                value = (rr.name.name, rr.type, rr.cls, rr.ttl, rdata)
                 database.fetchall(sqlStr, value)
 
+    # ID变换
     def transform(self, m):
         randID = random.randint(0, 65535)
         # 加入到消息转换字典中
-        lock.acquire()
+        dictLock.acquire()
         if self.clientAddress in idTransDict:
             idTransDict[self.clientAddress].append((m.header.id, randID, time()))# 字典记录消息id转换的对应关系,一个IP一个dict,一对消息转换对应其中一个键值对
         else:
             idTransDict[self.clientAddress] = []
             idTransDict[self.clientAddress].append((m.header.id, randID, time()))
         m.header.id = randID
-        lock.release()
+        dictLock.release()
         return m.toStr()
 
+    # ID反变换
     def inverseTransform(self, m):
-        lock.acquire()
+        dictLock.acquire()
         for addr, transList in idTransDict.items():
             for (front, back, timeStamp) in transList:
                 if m.header.id == back:
                     m.header.id = front
                     idTransDict.pop(addr)
-                    lock.release()
+                    dictLock.release()
                     return m.toStr(), addr, timeStamp
 
     def relay(self, message, addr):
         self.server.socket.sendto(message, addr)
-
+        
     def packMessage(self, message):  # 这里message是参考
         pass
 
@@ -113,10 +119,11 @@ if __name__ == "__main__":
     # setting = cmd[3]
 
     idTransDict = {}  # 消息ID转换的字典
-    lock = threading.Lock()
+    dictLock = threading.Lock()
+    varLock = threading.Lock()
     database = DNSDataBase(mincached=2, maxcached=5, maxconnections=10, database='DNSDataBase.db')
     # 开启一个线程用作UDP干活,似乎主线程很闲，没必要这么做哦
-    with UDPServer(('10.28.128.174', 60000), DnsHandler) as dnsServer:
+    with UDPServer((CLIENT, 60000), DnsHandler) as dnsServer:
         serverThread = threading.Thread(target = dnsServer.server_forever)
         serverThread.daemon = True
         serverThread.start()
