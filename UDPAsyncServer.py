f86303f028c53cb0bf5727036297132b248d26e8
tsotfsk <1337990880@qq.com>
2019-05-23 15:40:39 +0800

    精简server

--- UDPAsyncServer.py
+++ UDPAsyncServer.py
@@ -8,24 +8,23 @@ import sys
 import threading
 import logging
 
-from time import monotonic as time
+from time import monotonic as time2
 
 __all__ = ["UDPServer", "BaseRequestHandler"]
-_ServerSelector = selectors.SelectSelector
+
+ServerSelector = selectors.SelectSelector
 
 class UDPServer:
-
-    timeout = None
+    '''
+        一个采用多路I/O复用的UDP服务器
+    '''
     daemonThreads = False
-    _blockOnClose = False
-    _threads = None
+    threads = None
 
     def __init__(self, serverAddress, HandleClass):
 
         self.serverAddress = serverAddress
-        self.HandleClass = HandleClass  # 处理事务专用类
-        self.__isShutDown = threading.Event()
-        self.__isShutDownRequest = False
+        self.HandleClass = HandleClass  # 处理事务专用
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.maxPacketSize = 8192  # 一般socket缓冲区是8K
 
@@ -36,113 +35,76 @@ class UDPServer:
             raise
 
     def serverBind(self):
-        '''
-            绑定地址
-        '''
+
         self.socket.bind(self.serverAddress)
         self.serverAddress = self.socket.getsockname()
 
-    def server_forever(self, pollInterval = 0.5):
-
-        self.__isShutDown.clear()
+    def serverForever(self, pollInterval = 0.5):
+        '''
+            服务器启动,pollInterval指的是轮询的超时时间
+        '''
         try:
-            # XXX: polling is not good
-            with _ServerSelector() as selector:
+            # XXX: 轮询不好，而且实际上poll如果有多条消息到来还是串行读取，所以此处通过多线程来提高效率
+            with ServerSelector() as selector:
                 selector.register(self, selectors.EVENT_READ)
-                while not self.__isShutDownRequest:
+                while True:
                     ready = selector.select(pollInterval)
                     if ready:
-                        self._handleRequestNoblock()
-
-                        self.serviceActions()
-        finally:
-            self.__isShutDownRequest = False
-            self.__isShutDown.set()
+                        self.handleRequestNoblock()
+        except Exception as e:
+            logging.error(e)
 
     def fileno(self):
+        '''
+            select需要的文件描述符
+        '''
         return self.socket.fileno()
 
-    def shutdown(self):
-        
-        self.__isShutDownRequest = True
-        self.__isShutDown.wait()
-    
-    def serviceActions(self):
-        pass
-
-    def _handleRequestNoblock(self):
+    def handleRequestNoblock(self):
         try:
             request, clientAddress = self.getRequest()
         except OSError:
             return
         
-        if self.verifyRequest(request, clientAddress):
-            try:
-                self.processRequest(request, clientAddress)
-            except Exception:
-                self.handleError(request, clientAddress)
-                self.shutDownRequest(request)
-            except:
-                self.shutDownRequest(request)
-                raise
-        else:
-            self.shutDownRequest(request)
-
-    def handleTimeout(self):
-        pass
+        try:
+            self.processRequest(request, clientAddress)
+        except Exception as e:
+            logging.error(e)
 
     def getRequest(self):
         data, clientAddr = self.socket.recvfrom(self.maxPacketSize)
         return (data, self.socket), clientAddr
-
-    def verifyRequest(self, request, clientAddress):
-        return True
 
     def processRequestThread(self, request, client_address):
 
         logging.info('正在处理的线程ID:{}, 当前活跃线程数:{}'.format(threading.current_thread().ident, threading.active_count()))
         try:
             self.finishRequest(request, client_address)
-        except Exception:
-            self.handleError(request, client_address)
-        finally:
-            self.shutDownRequest(request)
+        except Exception as e:
+            logging.error(e)
 
     def processRequest(self, request, clientAddress):
-        """Start a new thread to process the request."""
+        """开启一个线程去处理消息"""
         t = threading.Thread(target = self.processRequestThread,
                              args = (request, clientAddress))
         t.daemon = self.daemonThreads
-        if not t.daemon and self._blockOnClose:
-            if self._threads is None:
-                self._threads = []
-            self._threads.append(t)
+        if not t.daemon :
+            if self.threads is None:
+                self.threads = []
+            self.threads.append(t)
         t.start()
 
     def serverClose(self):
-        if self._blockOnClose:
-            threads = self._threads
-            self._threads = None
-            if threads:
-                for thread in threads:
-                    thread.join()
+
+        # 阻塞式的关闭
+        threads = self.threads
+        self.threads = None
+        if threads:
+            for thread in threads:
+                thread.join()
 
     def finishRequest(self, request, clientAddress):
-        self.HandleClass(request, clientAddress, self)
-
-    def shutDownRequest(self, request):
-        self.closeRequest(request)
-
-    def closeRequest(self, request):
-        pass
-
-    def handleError(self, request, clientAddress):
-        print('-' * 40, file = sys.stderr)
-        print('Exception happened during processing of request from',
-            clientAddress, file = sys.stderr)
-        import traceback
-        traceback.print_exc()
-        print('-' * 40, file = sys.stderr)    
+        self.HandleClass(request, clientAddress, self) 
 
     def __enter__(self):
         return self
@@ -172,3 +134,10 @@ class BaseRequestHandler:
         pass
    
 
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG,
+                            # filename=argDict['filename'],
+                            format='%(asctime)s - %(filename)s-%(lineno)s[%(threadName)s] - %(levelname)s: %(message)s')
+    with UDPServer(('0.0.0.0', 53), BaseRequestHandler) as dnsServer:
+        logging.debug('running')
+        dnsServer.serverForever()
