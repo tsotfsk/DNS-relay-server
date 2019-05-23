5ff83471d802c1b6f5a7b85967d3a33c06a0889e
tsotfsk <1337990880@qq.com>
2019-05-24 01:54:07 +0800

    优化了一些逻辑，解决了完整性冲突

--- DataBase.py
+++ DataBase.py
@@ -2,6 +2,7 @@ import sqlite3
 import logging
 from DBUtils.PooledDB import PooledDB
 from DNSMessage import *
+from time import time
 
 class DataBase:
     
@@ -26,9 +27,18 @@ class DataBase:
         try:
             cursor.execute(sqlStr, value)
             result = cursor.fetchall()
-        except Exception as e:
-            logging.error(e)
-            result=[]
+        except:
+            try:
+                if 'insert' in sqlStr: # 插入出错，也就是不满足完整性约束，那么就采取更新的策略
+                    newSqlStr = 'update DNS set TTL = ? , RDATA = ?, TIMESTAMP = ? where Name = ? and TYPE = ? and CLASS = ? and RDATA = ?'
+                    newValue = (value[3], value[4], value[5], value[0], value[1], value[2], value[4])
+                    cursor.execute(newSqlStr, newValue)
+                    result = cursor.fetchall()
+                else:
+                    result=[]
+            except Exception as e:
+                result = []
+                logging.error(e)
         self.close(conn, cursor)
         return result
     
@@ -52,7 +62,7 @@ class DNSDataBase(DataBase):
                 CLASS       SMALLINT            CHECK(CLASS >= 0 AND CLASS <= 65535),
                 TTL              INT            CHECK(TTL >= 0),
                 RDATA           TEXT            NOT NULL,
-                TIMESTAMP       TEXT            NOT NULL,
+                TIMESTAMP     DOUBLE            CHECK(TIMESTAMP > 0),
                 PRIMARY KEY     (NAME, TYPE, CLASS, RDATA));''')
         except Exception as e:
             logging.error(e)
@@ -72,7 +82,29 @@ class DNSDataBase(DataBase):
             rr =  ResourceRecord(item[0].encode('ascii'), item[1], item[2], item[3], preference=int(rList[0]), exchange=rList[1].encode('utf-8'))
         return rr
 
+    def selectRR(self, curtime, sqlStr, value):
+        '''
+            自带删除的查询
+        '''
+        result = self.fetchall(sqlStr, value)
+        resultTemp = result[:]
+        for item in resultTemp:
+            if item[3] + item[5] < curtime:
+                self.deleteRR(item)
+                result.remove(item)
+                logging.debug('删除了一个资源记录{}'.format(tuple(item)))
+        return result
+
+    def deleteRR(self, item):
+        '''
+            主要是删除已经过期的消息
+        '''
+        sqlStr = 'delete from DNS where Name = ? and TYPE = ? and CLASS = ? and TTL = ? and RDATA = ? and TIMESTAMP = ?'
+        value = tuple(item)
+        self.fetchall(sqlStr, value)
+
 if __name__ == "__main__":
     database = DNSDataBase(mincached=0, maxcached=0, maxconnections=10, database='DNSDataBase.db')
     rr = database.toRR(('www.baidu.com', CNAME, IN, 201, 'www.a.fen.com'))
+
     
